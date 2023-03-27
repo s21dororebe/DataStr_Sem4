6ba3fa07f4184d3ff8f11413617969d4a13b448c
s21dororebe <s21dororebe@venta.lv>
2023-03-27 12:10:41 +0300

    Edit: MyGraph class (addEdge() - not finished)

--- src/datastr/MyGraph.java
+++ src/datastr/MyGraph.java
@@ -36,6 +36,17 @@ public class MyGraph <T> {
         arraySize = newArraySize;
     }
 
+    private int searchVertice(T inputVertice){
+        for(int i = 0; i < elementCounter; i++){
+            if(graphElements[i].getElement().equals(inputVertice)){
+                //return index of element
+                return i;
+            }
+        }
+        //return -1 if element is not there
+        return -1;
+    }
+
     public void addVertice(T inputElement) throws Exception {
         if(inputElement == null){
             throw (new Exception("Vertice is null"));
@@ -43,7 +54,7 @@ public class MyGraph <T> {
         //verify if vertice is not already in graph
         for(int i = 0; i < elementCounter; i++){
             if(graphElements[i].getElement().equals(inputElement)){
-                throw (new Exception("Vertice is already in graph"));
+                    throw (new Exception("Vertice is already in graph"));
             }
         }
         //verify if graph is not full
@@ -51,8 +62,27 @@ public class MyGraph <T> {
             increaseArray();
         graphElements[elementCounter++] = new MyVerticeNode<T>(inputElement);
     }
-    public boolean addEdge(T fromV, T toV, int weight){
+    public void addEdge(T elementFrom, T elementTo, int edgeWeight) throws Exception{
+        if(elementFrom == null && elementTo == null && edgeWeight <= 0){
+            throw (new Exception("Incorrect arguments"));
+        }
+        //verify if elementFrom and elementTo are real
+        int indexFrom= searchVertice(elementFrom);
+        int indexTo= searchVertice(elementTo);
+        //TODO add vertice if it is not found in graph
+        if(indexTo < 0 || indexFrom < 0)
+            throw (new Exception("One or both vertices are not in graph"));
 
+        MyEdgeNode newNode = new MyEdgeNode(indexTo, edgeWeight);
+        //if it is as first edge
+        if(graphElements[indexFrom].getFirstEdge()==null){
+            graphElements[indexFrom].setFirstEdge(newNode);
+        } else {
+            MyEdgeNode temp = graphElements[indexFrom].getFirstEdge();
+            while(temp.getNext()!=null)
+                temp = temp.getNext();
+            temp.setNext(newNode);
+        }
     }
     public void printNeighbourVertices(T fromV){}
     public void makeEmpty(){}
