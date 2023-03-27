00d315c7ee76e9206b40b778e1ee0a02b1040cba
s21dororebe <s21dororebe@venta.lv>
2023-03-27 11:04:58 +0300

    Edit: MyEdgeNode class - getters and setters

--- src/datastr/MyEdgeNode.java
+++ src/datastr/MyEdgeNode.java
@@ -1,2 +1,41 @@
-package datastr;public class MyEdgeNode {
+package datastr;
+
+public class MyEdgeNode {
+    private int indexOfVertice;
+    private float weight; //piemeram, kilometri
+    private MyEdgeNode next = null;
+
+    public int getIndexOfVertice() {
+        return indexOfVertice;
+    }
+    public void setIndexOfVertice(int inputIndexOfVertice) {
+        if(inputIndexOfVertice >= 0){
+            indexOfVertice = inputIndexOfVertice;
+        } else {
+            indexOfVertice = -1;
+        }
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+    public void setWeight(float inputWeight) {
+        if(inputWeight > 0 && inputWeight < 43000){
+            weight = inputWeight;
+        } else {
+            weight = 0;
+        }
+    }
+
+    public MyEdgeNode getNext() {
+        return next;
+    }
+    public void setNext(MyEdgeNode next) {
+        this.next = next;
+    }
+
+//    public MyEdgeNode(){
+//        setIndexOfVertice();
+//        setWeight();
+//    }
 }
---
+++ src/datastr/MyGraph.java
@@ -0,0 +1,29 @@
+package datastr;
+
+public class MyGraph<T> {
+/*
+    public boolean isFull(){}
+    public boolean isEmpty(){}
+    public boolean addVertice(T newItem){}
+    public boolean addEdge(T fromV, T toV, int weight){}
+    public int indexIs(T item){}
+    public int weightIs(T fromV, T toV){}
+    public void printNeighbourVertices(T fromV){}
+    public String toString(){}
+    public void makeEmpty(){}
+    //nevis izprintē, bet ievieto virsotni rindā un atgriež rindu
+    public MyQueue saveNeighbourVertices(T fromV){}
+    // atzīmē virsotni kā apmeklētu
+    public void markVertex(T vertex){}
+    // pārbauda, vai virsotne jau ir apmeklēta
+    public boolean isMarked(T vertex){}
+    public void clearMarks(){}
+    // grafa apiešana ejot dziļumā, pēc nepieciešamības var izmantot savas iepriekš izveidotās MyStack un MyQueue klases
+    public void printDepthFirst(T fromV, T toV){}
+    // grafa apiešana ejot platumā, pēc nepieciešamības var izmantot savas iepriekš izveidotās MyStack un MyQueue klases
+    public void printBreadthFirst(T fromV, T toV){}
+    //īsākā ceļa atrašana no virsotnes fromV uz virsotni toV
+    public void printShortestPath( T fromV, T toV){}
+    */
+
+}
